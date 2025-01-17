b6e28cc277dc66b2ad80ce5c800fc12727656d7b
Jason <jasnael94@gmail.com>
2025-01-17 09:52:38 +0100

    adding user & admin

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+// src/Controller/LoginController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Request;
+
+class LoginController extends AbstractController
+{
+    #[Route("/login", name:"login")]
+    public function login(Request $request)
+    {
+        // Si l'utilisateur est déjà authentifié, on le redirige
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
+        // Vérification des erreurs d'authentification
+        if ($request->attributes->get(Security::AUTHENTICATION_ERROR)) {
+            // Ajouter un message flash pour l'erreur
+            $this->addFlash('error', 'Identifiants incorrects.');
+        }
+
+        // Affichage de la page de connexion
+        return $this->render('security/login.html.twig');
+    }
+}
---
+++ tests/Controller/AuthControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class AuthControllerTest extends WebTestCase{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/auth');
+
+        self::assertResponseIsSuccessful();
+    }
+}
